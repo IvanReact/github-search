da69756ff3c42c9418d50c28020d0e4f591b412a
Ivan <susnicaivan90@gmail.com>
2022-05-01 22:32:10 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const drugariIzSkole = [
+  "NikolaV91",
+  "anamarelja",
+  "NensyN",
+  "nesawesly",
+  "dachoist",
+];
+
+const containerZaDrugare = document.querySelector("#drugariContainer");
+const containerZaRandomLjude = document.querySelector("#randomItLjudi");
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+function createRandom(user) {
+  user.items.forEach((e, i, arr) => {
+    const containerZaRandom = document.createElement("div");
+    containerZaRandom.classList.add("pojedinacniContainer");
+
+    const userImage = document.createElement("img");
+    userImage.setAttribute("src", e.avatar_url);
+    
+    userImage.classList.add("userImage");
+    userImage.addEventListener("click", () => {
+        localStorage.setItem('user',e.login);
+        window.location.href = "./user.html";
+      });
+
+
+    const userID = document.createElement("h3");
+    userID.innerHTML = `User: ${e.login}`;
+    userID.classList.add("userID");
+
+    containerZaRandomLjude.append(containerZaRandom);
+    containerZaRandom.append(userImage, userID);
+  });
+}
+
+function createDrugar(user) {
+  const containerZaDruga = document.createElement("div");
+  containerZaDruga.classList.add("pojedinacniContainer");
+
+  const userImage = document.createElement("img");
+  userImage.setAttribute("src", user.avatar_url);
+  userImage.classList.add("userImage");
+  userImage.addEventListener("click", () => {
+    localStorage.setItem('user',user.login);
+    window.location.href = "./user.html";
+  });
+
+  const userID = document.createElement("h3");
+  userID.innerHTML = `${user.login}`;
+  userID.classList.add("userID");
+
+  containerZaDrugare.append(containerZaDruga);
+  containerZaDruga.append(userImage, userID);
+}
+
+window.addEventListener("load", () => {
+  drugariIzSkole.forEach((e, i, arr) => {
+    fetch(`https://api.github.com/users/${e}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        createDrugar(data);
+      });
+  });
+});
+
+button.addEventListener("click", () => {
+  fetch(`https://api.github.com/search/users?per_page=${9}&q=${input.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      containerZaRandomLjude.innerHTML = "";
+      console.log(data);
+      createRandom(data);
+    });
+});
---
+++ user.js
@@ -0,0 +1,42 @@
+const getter = localStorage.getItem('user');
+const main = document.querySelector('main');
+
+window.addEventListener("load", () => {
+      fetch(`https://api.github.com/users/${getter}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          createDrugar(data);
+        });
+  });
+
+  function createDrugar(user) {
+    const containerZaSlikuDruga = document.createElement('div');
+    containerZaSlikuDruga.classList.add('containerZaSlikuDruga');
+ 
+    const userImage = document.createElement("img");
+    userImage.setAttribute("src", user.avatar_url);
+    
+// ===========================================================
+    const containerZaDruga = document.createElement("div");
+    containerZaDruga.classList.add('containerZaDruga');
+  
+    const userID = document.createElement("h2");
+    userID.innerHTML = `${user.name}`;
+    userID.classList.add("userID");
+  
+    const numberOfFollowers = document.createElement("h3");
+    numberOfFollowers.innerHTML = `Followers: ${user.followers}`;
+
+    const following = document.createElement("h3");
+    following.innerHTML = `Following: ${user.following}`;
+
+    const bio = document.createElement("h3");
+    bio.innerHTML = `Bio: ${user.bio}`;
+
+ //===========================================================
+    main.append(containerZaSlikuDruga);
+    containerZaSlikuDruga.append(userImage);
+    main.append(containerZaDruga);
+    containerZaDruga.append( userID, numberOfFollowers,following,bio);
+  }
